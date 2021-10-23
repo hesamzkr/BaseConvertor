e56d8b9f38e3da0b0d792a4a0b60ed5a3daec5cf
Krish <75043245+Krish-sysadmin@users.noreply.github.com>
2021-10-23 19:36:16 +0200

    updated la programa

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-BASE = int(input("Enter base you want to convert to : "))
+import math
 
+BASE = int(input("Enter base you want to convert to: "))
 
 def to_normal(number):
     if (number):
         output = 0
         for i in range(len(number) - 1, -1, -1):
             output += BASE ** (len(number) - i - 1) * int(str(number[i]))
-        print("Base10:", output)
+        print("Base 10:", output)
 
-
-def to_base(number_str):
-    if (number_str):
-        output = []
-        number = int(number_str)
-        n = number / BASE
-        while n >= 1:
-            x = n % 1
-            output.append(x * BASE)
-            n = int(n) / BASE
-        
-        x = n % 1
-        output.append(x * BASE)
-
-        print(f"\033[92m--------\033[0m\n{' '.join(str(round(e)) for e in output)[::-1]}")
-
+def to_base(number_str, base=BASE):
+    number = int(number_str)
+    if number == 0:
+        return [0]
+    thedigits = []
+    while number:
+          digits.append(int(number % base))
+          number = math.floor(number/base)
+    return thedigits[::-1]
 
 def main():
-    print(f"\033[96mBase {BASE} Convertor\033[0m")
+    print(f"\033[1;31;40mBase {BASE} Convertor\033[0m")
     while True:
-        number = input("Number: ")
-        to_base(number)
+        number = input("Input the number: ")
+        print(to_base(number))
         # to_normal(number)
         print("\033[92m--------\033[0m")
 
